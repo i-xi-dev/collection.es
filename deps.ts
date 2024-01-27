cd22651edae5157ac94d4e681284283e22a00da4
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-27 22:41:18 +0900

    2.0.10

--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export { SafeInteger } from "https://raw.githubusercontent.com/i-xi-dev/number.es/6.0.0/mod.ts";
+export { SafeInteger } from "https://raw.githubusercontent.com/i-xi-dev/number.es/7.0.0/mod.ts";
